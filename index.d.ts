b3fa98cf236f3e761844866232fc6f06021c0579
Antonio Budiselić <antonio.budiselic@gmail.com>
2023-08-15 21:27:07 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,14 @@ declare module 'nestjs-cloud-logging' {
   export const WinstonLogger: unique symbol;
   export const WinstonLoggerService: unique symbol;
   export const WinstonLoggerServiceApp: unique symbol;
+
+  export interface WinstonLoggerModuleOptions {
+    projectId: string;
+    keyFilename: string;
+    isGlobal?: boolean;
+  }
+
+  export class WinstonLoggerModule {
+    static forRoot(options: WinstonLoggerModuleOptions): DynamicModule;
+  }
 }
